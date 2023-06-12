83efb1fdb125a70b5fffa927a62c711dc1f72665
hOlrrDtr <danylo.shporta@nure.ua>
2023-06-12 22:16:32 +0300

    article create

--- main.py
+++ main.py
@@ -179,6 +179,22 @@ class NewArticleScreen(Screen):
         headline = self.ids.headline.text
         article_text = self.ids.article_text.text
         tags = self.ids.tags.text
+        data = {
+            "creator": 19,
+            'content_type': "article",
+            'like_count': 0,
+            'is_paid': False
+        }
+        r = requests.post("http://127.0.0.1:8000/content/create", json=data)
+        data = {
+            "content_id": r.json()["id"],
+            "article_name": headline,
+            "text": article_text,
+            "keywords": tags
+
+        }
+        r = requests.post("http://127.0.0.1:8000/article/create", json=data)
+
 
 
 class LifeHealther(MDApp):
