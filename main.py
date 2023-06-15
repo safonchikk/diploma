950f3bbe778bb44cf081d629a2dbf37673a1cebe
hOlrrDtr <danylo.shporta@nure.ua>
2023-06-15 18:37:39 +0300

    video create

--- main.py
+++ main.py
@@ -233,6 +233,27 @@ class NewVideoScreen(Screen):
         tags = self.ids.tags.text
         video_path = self.chosen_video
         thumbnail_path = self.chosen_thumbnail
+        files = {}
+        video_file = open(video_path, 'rb')
+        files['video'] = video_file
+        f = open(thumbnail_path, 'rb')
+        files['preview'] = f
+        content_data = {
+            "creator": 14,
+            "content_type": "video",
+            "like_count": 0,
+            "is_paid": False
+        }
+        r = requests.post("http://127.0.0.1:8000/content/create", json=content_data)
+        data = {
+            "content_id": r.json()["id"],
+            "video_name": title,
+            "keywords": tags
+        }
+        r = requests.post("http://127.0.0.1:8000/video/create", data=data, files=files)
+        f.close()
+        video_file.close()
+
 
 
 class LifeHealther(MDApp):
