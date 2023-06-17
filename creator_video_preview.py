377e61b71629de2d0ed99a28a87ba94280e549a4
hOlrrDtr <danylo.shporta@nure.ua>
2023-06-17 15:16:17 +0300

    delete video

--- creator_video_preview.py
+++ creator_video_preview.py
@@ -1,12 +1,15 @@
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
+import requests
 
 
 class CreatorVideoPreview(Button):
-    def __init__(self, thumbnail, title='Title', **kwargs):
+    def __init__(self, thumbnail, content_id, title='Title', **kwargs):
         super(CreatorVideoPreview, self).__init__(**kwargs)
+        self.content_id = content_id
         self.ids.thumbnail.texture = thumbnail
         self.ids.title.text = title
 
     def delete(self):
-        pass
+        requests.delete("http://127.0.0.1:8000/video/delete/" + str(self.content_id))
+        requests.delete("http://127.0.0.1:8000/content/" + str(self.content_id)+"/delete")
--- video_page.py
+++ video_page.py
@@ -19,26 +19,26 @@ class VideoPageScreen(MyScreen):
         self.load_videos()
 
     def load_videos(self):
-        for i in range(5):
-            videos = requests.get("https://lifehealther.onrender.com/video/creator/14")
-            for i in videos.json().values():
-                url = "https://lifehealther.onrender.com/video/info/" + str(i["id"])
-                video_info = requests.get(url)
-                video_info = video_info.json()
-                decoded_bytes = base64.b64decode(video_info["preview"])
-                temp_filename = 'temp_image.png'
-                with open(temp_filename, 'wb') as file:
-                    file.write(decoded_bytes)
+        videos = requests.get("https://lifehealther.onrender.com/video/creator/14")
+        for i in videos.json().values():
+            url = "https://lifehealther.onrender.com/video/info/" + str(i["id"])
+            video_info = requests.get(url)
+            video_info = video_info.json()
+            decoded_bytes = base64.b64decode(video_info["preview"])
+            temp_filename = 'temp_image.png'
+            with open(temp_filename, 'wb') as file:
+                file.write(decoded_bytes)
 
-                # Створення об'єкта CoreImage з тимчасового зображення
-                core_image = CoreImage(temp_filename)
-                url = "https://lifehealther.onrender.com/user/" + str(i["creator"])
-                creator_info = requests.get(url)
-                creator_info = creator_info.json()
-                video_preview = CreatorVideoPreview(size_hint_y=None,
-                                             height=dp(300),
-                                             thumbnail=core_image.texture,
-                                             title=video_info["video_name"]
-                                             )
-                os.remove(temp_filename)
-            self.ids.videos_grid.add_widget(video_preview)
+            # Створення об'єкта CoreImage з тимчасового зображення
+            core_image = CoreImage(temp_filename)
+            url = "https://lifehealther.onrender.com/user/" + str(i["creator"])
+            creator_info = requests.get(url)
+            creator_info = creator_info.json()
+            video_preview = CreatorVideoPreview(size_hint_y=None,
+                                                height=dp(300),
+                                                thumbnail=core_image.texture,
+                                                content_id=i["id"],
+                                                title=video_info["video_name"],
+                                                )
+            os.remove(temp_filename)
+        self.ids.videos_grid.add_widget(video_preview)
