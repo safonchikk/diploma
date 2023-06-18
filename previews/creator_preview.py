5c300d3ada5399a2d17020d29c593ad091277652
safon <safonchik.jn@gmail.com>
2023-06-19 01:53:18 +0300

    creator preview

--- previews/creator_preview.py
+++ previews/creator_preview.py
@@ -2,4 +2,7 @@ from kivy.uix.button import Button
 
 
 class CreatorPreview(Button):
-    pass
+    def __init__(self, name, avatar, **kwargs):
+        super(CreatorPreview, self).__init__()
+        self.ids.name.text = name
+        self.ids.avatar.texture = avatar
