02056d63f381881303be0d6e72a972ad02daa15d
hOlrrDtr <danylo.shporta@nure.ua>
2023-06-18 15:30:23 +0300

    fix

--- creator_edit_profile.py
+++ creator_edit_profile.py
@@ -1,12 +1,17 @@
 from kivymd.uix.filemanager import MDFileManager
+from kivy.core.image import Image as CoreImage
 
 from my_screen import MyScreen
+import requests
+import base64
+import os
 
 
 class CreatorEditProfile(MyScreen):
     def __init__(self, **kwargs):
         super(CreatorEditProfile, self).__init__(**kwargs)
         self.chosen_avatar = ''
+        self.k = 0
         self.file_manager = MDFileManager(
             select_path=self.select_path,
             exit_manager=self.exit_manager
@@ -14,12 +19,25 @@ class CreatorEditProfile(MyScreen):
 
     #тут завантаж старі теги і аву
     def load_current_info(self):
+        creator_content = requests.get("https://lifehealther.onrender.com/creator/info/14").json()
+        creator_mongo = requests.get('https://lifehealther.onrender.com/creator/mongo/14').json()
         self.ids.save_button.disabled = True
+        if creator_mongo["avatar"] == "no":
+            self.ids.new_avatar.source = 'images/account.png'
+        else:
+            decoded_bytes = base64.b64decode(creator_mongo["avatar"])
+            temp_filename = 'temp_image' + str(self.k) + '.png'
+            with open(temp_filename, 'wb') as file:
+                file.write(decoded_bytes)
+            # Створення об'єкта CoreImage з тимчасового зображення
+            core_image = CoreImage(temp_filename)
+            self.ids.new_avatar.texture = core_image.texture
+
         self.chosen_avatar = ''
         self.ids.avatar_path.text = ''
         #отут
-        self.ids.new_avatar.source = 'images/account.png'
-        self.ids.info.text = ''
+
+        self.ids.info.text = creator_content["info"]
 
     @staticmethod
     def open_manager(file_manager):
@@ -37,7 +55,18 @@ class CreatorEditProfile(MyScreen):
         self.exit_manager(self.file_manager)
 
     def save_avatar(self):
-        pass
+        avatar_path = self.chosen_avatar
+        files = {}
+        f = open(avatar_path, 'rb')
+        files['avatar'] = f
+        r = requests.put("https://lifehealther.onrender.com/creator/update/avatar/14", files=files)
+        f.close()
+
 
     def save_info(self):
-        pass
+        info = self.ids.info.text
+        data = {
+            "info": info
+        }
+        requests.put("https://lifehealther.onrender.com/creator/14/update", data=data )
+
