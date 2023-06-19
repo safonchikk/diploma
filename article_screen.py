58f704bd9e7ad017dca93fc2f2509cc19dfe8acd
safon <safonchik.jn@gmail.com>
2023-06-19 03:54:15 +0300

    article

--- article_screen.py
+++ article_screen.py
@@ -4,5 +4,8 @@ from my_screen import MyScreen
 class ArticleScreen(MyScreen):
     def __init__(self, content_id, **kwargs):
         super(ArticleScreen, self).__init__(name='article', **kwargs)
-
+        #self.ids.author.text =
+        #self.ids.text.text =
+        #self.ids.headline.text =
+        #self.ids.author_avatar.texture =
         ...
