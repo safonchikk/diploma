3c9fbd25b138f20958d5157deea69196bb7e9e4f
hOlrrDtr <danylo.shporta@nure.ua>
2023-06-19 12:53:58 +0300

    fix

--- main_screen.py
+++ main_screen.py
@@ -65,19 +65,18 @@ class MainScreen(MyScreen):
         self.manager.current = 'article'
 
     def load_videos(self):
-        logging.basicConfig(level=logging.DEBUG)
         layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
         layout.bind(minimum_height=layout.setter('height'))
-        response = requests.get("https://lifehealther.onrender.com/video/30", stream=True)
-        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.mp4')
-        file_path = temp_file.name
-
-        with open(file_path, 'wb') as f:
-            for chunk in response.iter_content(chunk_size=1024):
-                f.write(chunk)
-
-        # Вставка відео у VideoPlayer
-        self.ids.video.source = file_path
+        # response = requests.get("https://lifehealther.onrender.com/video/30", stream=True)
+        # temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.mp4')
+        # file_path = temp_file.name
+        #
+        # with open(file_path, 'wb') as f:
+        #     for chunk in response.iter_content(chunk_size=1024):
+        #         f.write(chunk)
+        #
+        # # Вставка відео у VideoPlayer
+        # self.ids.video.source = file_path
         videos = requests.get("https://lifehealther.onrender.com/video/free")
         for i in videos.json().values():
             url = "https://lifehealther.onrender.com/video/info/" + str(i["id"])
