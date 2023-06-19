c2d961d19b02ce45f68c7d877a1499a88ac71515
hOlrrDtr <danylo.shporta@nure.ua>
2023-06-19 15:53:02 +0300

    fix

--- short_page.py
+++ short_page.py
@@ -23,29 +23,27 @@ class ShortPageScreen(MyScreen):
     def load_videos(self):
         self.ids.videos_grid.clear_widgets()
         creator_id = MDApp.get_running_app().user
-        for i in range(5):
-            videos = requests.get("https://lifehealther.onrender.com/video/creator/14")
+        videos = requests.get("https://lifehealther.onrender.com/short/creator/" + str(creator_id))
+        if videos.json() != {}:
             for i in videos.json().values():
-                url = "https://lifehealther.onrender.com/video/info/" + str(i["id"])
+                url = "https://lifehealther.onrender.com/short/info/" + str(i["id"])
                 video_info = requests.get(url)
                 video_info = video_info.json()
                 decoded_bytes = base64.b64decode(video_info["preview"])
-                temp_filename = 'temp_image.png'
+                temp_filename = 'shor_creator_image'+str(self.k)+'.png'
                 with open(temp_filename, 'wb') as file:
                     file.write(decoded_bytes)
 
                 # Створення об'єкта CoreImage з тимчасового зображення
                 core_image = CoreImage(temp_filename)
-                url = "https://lifehealther.onrender.com/user/" + str(i["creator"])
-                creator_info = requests.get(url)
-                creator_info = creator_info.json()
                 short_preview = ShortPreview(size_hint_y=None,
                                              height=dp(300),
                                              thumbnail=core_image.texture,
-                                             title=video_info["video_name"]
+                                             title=video_info["video_name"],
+                                             content_id=i["id"]
                                              )
                 os.remove(temp_filename)
-            # self.ids.videos_grid.add_widget(short_preview)
+                self.ids.videos_grid.add_widget(short_preview)
 
     def create_add(self):
         self.manager.add_widget(NewShortScreen(name='new_short'))
