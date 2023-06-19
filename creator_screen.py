b0c02dd94ee5496cda991df439918732eca238ad
safon <safonchik.jn@gmail.com>
2023-06-19 04:39:01 +0300

    creator

---
+++ creator_screen.py
@@ -0,0 +1,11 @@
+from my_screen import MyScreen
+
+
+class CreatorScreen(MyScreen):
+    def __init__(self, content_id, **kwargs):
+        super(CreatorScreen, self).__init__(name='creator', **kwargs)
+        #self.ids.author.text =
+        #self.ids.text.text =
+        #self.ids.headline.text =
+        #self.ids.author_avatar.texture =
+        ...
