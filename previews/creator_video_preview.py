11a156af614c91dc5ee378f87efc2810ee6ab947
safon <safonchik.jn@gmail.com>
2023-06-20 16:49:39 +0300

    popup on video delete

--- previews/creator_video_preview.py
+++ previews/creator_video_preview.py
@@ -1,5 +1,7 @@
 from kivy.uix.button import Button
 import requests
+from kivymd.uix.button import MDRoundFlatButton
+from kivymd.uix.dialog import MDDialog
 
 from author_pages.update_video_screen import UpdateVideoScreen
 
@@ -11,10 +13,26 @@ class CreatorVideoPreview(Button):
         self.ids.thumbnail.texture = thumbnail
         self.ids.title.text = title
         self.create_upd = create_upd
+        self.dialog = None
 
     def delete(self):
+        title = "Are you sure?"
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title=title,
+                buttons=[
+                    MDRoundFlatButton(text="Cancel", on_release=self.close_popup),
+                    MDRoundFlatButton(text="Yes", on_release=self.confirmed_delete),
+                ],
+            )
+        self.dialog.open()
+
+    def close_popup(self, instance):
+        self.dialog.dismiss()
+
+    def confirmed_delete(self):
         requests.delete("https://lifehealther.onrender.com/video/delete/" + str(self.content_id))
-        requests.delete("https://lifehealther.onrender.com/content/" + str(self.content_id)+"/delete")
+        requests.delete("https://lifehealther.onrender.com/content/" + str(self.content_id) + "/delete")
 
     def create_update(self):
         screen = UpdateVideoScreen(self.content_id, name='update_video')
