f2e19fd8fb5d365fdd8b52de751bdd401b24c860
hOlrrDtr <danylo.shporta@nure.ua>
2023-06-20 13:14:50 +0300

    fix

--- article_screen.py
+++ article_screen.py
@@ -1,5 +1,6 @@
 from my_screen import MyScreen
 from kivy.core.image import Image as CoreImage
+from kivymd.app import MDApp
 import requests
 import base64
 import os
@@ -8,13 +9,20 @@ import os
 class ArticleScreen(MyScreen):
     def __init__(self, content_id, **kwargs):
         super(ArticleScreen, self).__init__(name='article', **kwargs)
+        self.customer_id = MDApp.get_running_app().user
         url = "https://lifehealther.onrender.com/article/" + str(content_id)
         article_info = requests.get(url)
         article_info = article_info.json()
         content_info = requests.get("https://lifehealther.onrender.com/content/" + str(content_id)).json()
         creator_id = content_info["creator"]
+        self.content_id = content_id
         creator_mongo = requests.get('https://lifehealther.onrender.com/creator/mongo/' + str(creator_id)).json()
         user = requests.get('https://lifehealther.onrender.com/user/' + str(creator_id)).json()
+        liked_r = requests.get("https://lifehealther.onrender.com/content_like/" + str(content_id) + "/" + str(self.customer_id))
+        if liked_r.status_code == 404:
+            self.liked = False
+        elif liked_r.status_code == 200:
+            self.liked = True
         if creator_mongo["avatar"] == "NO":
             self.ids.author_avatar.source = 'images/account.png'
         else:
@@ -29,7 +37,21 @@ class ArticleScreen(MyScreen):
         self.ids.author.text = user["username"]
         self.ids.text.text = article_info["text"]
         self.ids.headline.text = article_info["article_name"]
+        self.like_count = content_info["like_count"]
 
     def like(self):
-        ...
-        #self.content_id
+        if self.liked:
+            r = requests.delete(
+                "https://lifehealther.onrender.com/content_like/delete/" +
+                str(self.content_id) + "/" + str(self.customer_id))
+            self.liked = False
+            self.like_count -= 1
+        else:
+            data = {
+                "content_id": int(self.content_id),
+                "customer_id": int(self.customer_id)
+            }
+            r = requests.post("https://lifehealther.onrender.com/content_like/create", json=data)
+            self.liked = True
+            self.like_count += 1
+
